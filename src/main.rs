e2062954da165842e6d6ffb4eb2bda6385cd2c12
demoth <bubnov.d.e@gmail.com>
2022-12-21 21:05:44 +0100

    cpu usage based coloring (red, yellow, green)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::io;
 use std::io::{Read, Write};
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
+use nix::sys::sysinfo::{sysinfo, SysInfo};
+
+extern crate num_cpus;
 
 fn main() -> io::Result<()> {
+    let cpu_count = num_cpus::get();
+
     let mut buffer = [0; 1024];
     let mut stdout = StandardStream::stdout(ColorChoice::Always);
 
@@ -18,6 +23,20 @@ fn main() -> io::Result<()> {
             break;
         }
 
+        // Get the current system information
+        let sys_info: SysInfo = sysinfo().unwrap();
+        // Calculate the current (1 minute average) CPU load as a percentage
+        let cpu_load = sys_info.load_average().0 as f32 / cpu_count as f32 * 100.0;
+
+        // Set the color of the output based on the CPU load
+        if cpu_load > 75.0 {
+            stdout.set_color(ColorSpec::new().set_fg(Some(Color::Red)))?;
+        } else if cpu_load > 50.0 {
+            stdout.set_color(ColorSpec::new().set_fg(Some(Color::Yellow)))?;
+        } else {
+            stdout.set_color(ColorSpec::new().set_fg(Some(Color::Green)))?;
+        }
+        print!("cpu load: {}", cpu_load);
         // Write the bytes we read to standard output
         let _ = io::stdout().write(&buffer[..bytes_read])?;
     }
