9d7bd8260cf41a74742fc14d9d94299797fb4d5b
demoth <bubnov.d.e@gmail.com>
2022-12-21 20:20:15 +0100

    add colored output (single color)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::io;
 use std::io::{Read, Write};
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 fn main() -> io::Result<()> {
     let mut buffer = [0; 1024];
+    let mut stdout = StandardStream::stdout(ColorChoice::Always);
+
+    // Set the color of the output to red
+    stdout.set_color(ColorSpec::new().set_fg(Some(Color::Red)))?;
 
     // Read input into the buffer in a loop until we reach the end of the input stream
     loop {
@@ -16,6 +21,6 @@ fn main() -> io::Result<()> {
         // Write the bytes we read to standard output
         let _ = io::stdout().write(&buffer[..bytes_read])?;
     }
+
     Ok(())
 }
-
