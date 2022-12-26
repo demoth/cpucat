43ee633112f4104d0a86998606172d74875844e4
Filipp <samoylovfp@gmail.com>
2022-12-26 13:15:12 +0200

    Get the number of cpu cores from sysinfo

--- src/bin/ramp_load.rs
+++ src/bin/ramp_load.rs
@@ -1,24 +1,29 @@
 use std::time::{Duration, Instant};
 
+use sysinfo::SystemExt;
+
 /// Generates linearly increasing and then decreasing load
 /// Cpu load graph is supposed to look like a step-wise approximation of a triangle wave
 fn main() {
     let step_width = Duration::from_millis(200);
-    let cpu_num = 24;
+    
+    let multithreading = 2;
+    let thread_num = sysinfo::System::new().physical_core_count().unwrap_or(16) as u32 * multithreading;
+    
     println!(
         "Gradually increasing and then decreasing CPU load, period is {:?}",
-        step_width * cpu_num * 2
+        step_width * thread_num * 2
     );
     loop {
         // Ramp up
-        for i in 0..cpu_num {
-            let load_duration = step_width * (cpu_num - i) * 2;
+        for i in 0..thread_num {
+            let load_duration = step_width * (thread_num - i) * 2;
             generate_load_in_bg_for(load_duration);
             std::thread::sleep(step_width);
         }
 
         // Ramp down (threads running to conclusion)
-        std::thread::sleep(step_width * cpu_num);
+        std::thread::sleep(step_width * thread_num);
 
         println!("This was a single load cycle to test how well cpucat can color this line")
     }
